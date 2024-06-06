de5f063227eab64648c744bf40a9da708cc782d6
Rubaeid-Sanjid <rubaeid.sanjid@gmail.com>
2024-06-07 00:29:00 +0600

    get scholarships api

--- index.js
+++ index.js
@@ -4,19 +4,55 @@ const app = express();
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 const jwt = require("jsonwebtoken");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 //middleware
 app.use(cors());
 app.use(express.json());
 
-app.post("/jwt", async (req, res) => {
-  const userEmail = req.body;
-  const token = jwt.sign(userEmail, process.env.ACCESS_TOKEN_SECRET, {
-    expiresIn: "1d",
-  });
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.b8fibtq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-  res.send({ token });
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
 });
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+
+    const scholarshipCollection = client
+      .db("eduScholar")
+      .collection("scholarships");
+
+    app.get('/scholarships', async(req, res)=>{
+        const result = await scholarshipCollection.find().toArray();
+        res.send(result);
+    })
+
+    app.post("/jwt", async (req, res) => {
+      const userEmail = req.body;
+      const token = jwt.sign(userEmail, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1d",
+      });
+
+      res.send({ token });
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("EduScholar server");
