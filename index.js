bf5318e4eb6a2c583b41363acf378c72100fe50a
Rubaeid-Sanjid <rubaeid.sanjid@gmail.com>
2024-06-06 23:52:55 +0600

    token created

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+require("dotenv").config();
 const port = process.env.PORT || 5000;
-require('dotenv').config()
+const jwt = require("jsonwebtoken");
 
 //middleware
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-app.get('/', (req, res)=>{
-    res.send('EduScholar server')
-})
+app.post("/jwt", async (req, res) => {
+  const userEmail = req.body;
+  const token = jwt.sign(userEmail, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: "1d",
+  });
 
-app.listen(port, ()=>{
-    console.log(`Server is running at port ${port}`);
-})
+  res.send({ token });
+});
+
+app.get("/", (req, res) => {
+  res.send("EduScholar server");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at port ${port}`);
+});
